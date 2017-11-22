42399154a9e30d4a7de1f39bdab2ec173c29de2c
Nicolas Papanicolaou <nicolas.papanicolaou20@gmail.com>
2017-11-21 20:37:50 -0500

    Grandement changer mais foncitonnelle pour plein de chose

--- tp2/client.c
+++ tp2/client.c
@@ -4,49 +4,135 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <wait.h>
+
 #define COLOR_YELLOW  "\x1B[33m"
 #define COLOR_WHITE  "\x1B[37m"
+
+char *concat(const char *, const char *);
+
+char *itoa(int i, char b[]);
+
 int main() {
 
     int fileDescriptor;
 
+    char *serveurSrc = "serveurP";
     char *pipeSrc = "pipe";
 
 
     char buffer[250];
     char buffer2[250];
 
+    int numeroRequete = 1;
+
+    char *pipeName;
+    int index = 0;
+    char indexString[100];
+    while (1) {
+        itoa(index, indexString);
+        pipeName = concat(pipeSrc, indexString);
+
+        if (access(pipeName, F_OK) == -1) {
+            printf("Pipe non existant\n");
+            break;
+        } else {
+            printf("Pipe existant\n");
+            index++;
+        }
+    }
+
+
 
     while (1) {
-        if (access(pipeSrc, F_OK) == -1) {
+        if (access(serveurSrc, F_OK) == -1) {
             printf("Serveur fermer\n");
             exit(0);
         } else {
             printf("Connection serveur established\n");
         }
 
-        fileDescriptor = open(pipeSrc, O_WRONLY);
+        mkfifo(pipeName, 0666);
+
+
+        printf("Numero requete : %d\n",numeroRequete);
         printf("Ecrire commande: \n");
         fgets(buffer, 250, stdin);
 
-        write(fileDescriptor, buffer, strlen(buffer) + 1);
+        if (strcmp(buffer,"\n") == 0){
+            remove(pipeName);
+            exit(0);
+        } else if (strcmp(buffer,"fin\n") == 0){
+            printf("Vous avez envoye la commande de fin de serveur");
+            remove(pipeName);
+            exit(0);
+        }
+
+        if (access(serveurSrc, F_OK) == -1) {
+            printf("Serveur fermer pendant votre input\n");
+            exit(0);
+        }
+
+        fileDescriptor = open(serveurSrc, O_WRONLY);
+
+
+
+        char *message = concat(pipeName, ",");
+
+
+        char temp[250];
+        strcpy(temp, message);
+
+        char bufferToSend[250];
+        char *temp2 = concat(temp, buffer);
+
+        stpcpy(bufferToSend, temp2);
+
+        write(fileDescriptor, bufferToSend, strlen(bufferToSend) + 1);
         close(fileDescriptor);
+        sleep(1);
+        numeroRequete++;
 
-        if (strcmp(buffer, "fin\n") == 0) {
-            printf("%s====================================\n", COLOR_YELLOW);
-            printf("Le serveur a recu la commande de fermer\n");
-            printf("====================================\n%s", COLOR_WHITE);
-            break;
-        } else {
 
-            fileDescriptor = open(pipeSrc, O_RDONLY);
-            read(fileDescriptor, buffer2, sizeof(buffer2));
-            printf("%s====================================\n", COLOR_YELLOW);
-            printf("%s", buffer2);
-            printf("====================================\n%s", COLOR_WHITE);
+        while (1) {
+            fileDescriptor = open(pipeName, O_RDONLY);
+            read(fileDescriptor, buffer2, 250);
+
+            printf("%s\n", buffer2);
+
             close(fileDescriptor);
+            break;
         }
     }
 
+
     return 0;
+}
+
+char *concat(const char *s1, const char *s2) {
+    char *result = malloc(strlen(s1) + strlen(s2) + 1);//+1 for the null-terminator
+    //in real code you would check for errors in malloc here
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+char *itoa(int i, char b[]) {
+    char const digit[] = "0123456789";
+    char *p = b;
+    if (i < 0) {
+        *p++ = '-';
+        i *= -1;
+    }
+    int shifter = i;
+    do {
+        ++p;
+        shifter = shifter / 10;
+    } while (shifter);
+    *p = '\0';
+    do {
+        *--p = digit[i % 10];
+        i = i / 10;
+    } while (i);
+    return b;
 }
--- tp2/serveur.c
+++ tp2/serveur.c
@@ -1,44 +1,78 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdio.h>
 
 #define COLOR_YELLOW  "\x1B[33m"
 #define COLOR_WHITE  "\x1B[37m"
+#define COLOR_GREEN  "\x1B[32m"
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN    "\x1b[36m"
 
 
 int main() {
 
-    int fileDescriptor;
 
-    char *pipeSrc = "pipe";
+    int fdREad;
+    int fdWrite;
 
-    mkfifo("pipe", 0666);
+    char *pipeSrc = "serveurP";
+
+    mkfifo("serveurP", 0666);
 
     char buffer[250];
+    int count = 1;
+    printf("%s> Serveur up\n%s",COLOR_CYAN,COLOR_WHITE); int i = 0;
+    fdREad = open(pipeSrc, O_RDONLY);
 
     while (1) {
-        fileDescriptor = open(pipeSrc, O_RDONLY);
-        read(fileDescriptor, buffer, 250);
+        printf("%s> Connection established - Request number : %d\n%s",COLOR_CYAN, count,COLOR_WHITE);
+        count++;
+        read(fdREad, buffer, 250);
 
-        if (strcmp(buffer, "fin\n") == 0) {
-            printf("%s====================================\n", COLOR_YELLOW);
-            printf("Fin du Serveur\n");
-            printf("====================================\n%s", COLOR_WHITE);
-            close(fileDescriptor);
-            remove(pipeSrc);
-            break;
+        printf("%s\n", buffer);
+
+
+        char nomPipe[250];
+        char commande[250];
+        int pipeNameDone = -1;
+        int i, j;
+        for (i = 0, j = 0; i < strlen(buffer); i++) {
+            if (buffer[i] == ',') {
+                nomPipe[j + 1] = '\0';
+                j = 0;
+                pipeNameDone = 0;
+            } else {
+                if (pipeNameDone == 0) {
+                    commande[j] = buffer[i];
+
+                } else {
+                    nomPipe[j] = buffer[i];
+
+                }
+                j++;
+            }
+        }
+        commande[j] = '\0';
+        printf("%s> NomPipe = %s\n%s",COLOR_CYAN,nomPipe,COLOR_WHITE);
+        printf("%s> Commande = %s\n%s",COLOR_CYAN,commande,COLOR_WHITE);
+        if (strcmp(commande,"fin\n") == 0){
+            printf("%s> Fermeture du serveur\n%s",COLOR_CYAN,COLOR_WHITE);
+            sleep(1);
+            close(fdREad);
+            exit(0);
         } else {
-            close(fileDescriptor);
-            fileDescriptor = open(pipeSrc, O_WRONLY);
-            printf("La commande est : %s\n", buffer);
-            char *message = "Jai recu la commande!\n";
-            write(fileDescriptor, message, strlen(message) + 1);
-            close(fileDescriptor);
+            fdWrite = open(nomPipe, O_WRONLY);
+            char * message = "Jai recu la commande";
+            printf("Jai recu la commande et cest %s\n",commande);
+            write(fdWrite,message,strlen(message) + 1);
         }
     }
 
     return 0;
 }
+
